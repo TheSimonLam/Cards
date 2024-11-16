12fbab139d71c53ea3a54989f241a680e0600670
Simon Lam <simon.lam@victorianplumbing.co.uk>
2024-11-16 17:34:56 +0000

    cleaning up

--- app/(tabs)/earn.tsx
+++ app/(tabs)/earn.tsx
@@ -3,11 +3,11 @@ import { StyleSheet } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
-export default function ExploreScreen() {
+export default function EarnScreen() {
   return (
     <>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">Earn</ThemedText>
       </ThemedView>
       <ThemedText>
         This app includes example code to help you get started.
--- app/(tabs)/friends.tsx
+++ app/(tabs)/friends.tsx
@@ -3,11 +3,11 @@ import { StyleSheet } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
-export default function ExploreScreen() {
+export default function FriendsScreen() {
   return (
     <>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">Friends</ThemedText>
       </ThemedView>
       <ThemedText>
         This app includes example code to help you get started.
