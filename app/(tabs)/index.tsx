6bb7600395bc4ce44815aab2ec7cc07df0411578
Simon Lam <simon.lam@victorianplumbing.co.uk>
2024-11-18 10:17:48 +0000

    proggers

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,18 +2,22 @@ import { StyleSheet, Platform } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { Colors } from "@/constants/Colors";
 
 export default function HomeScreen() {
   return (
-    <>
+    <ThemedView
+      lightColor={Colors.light.background}
+      darkColor={Colors.dark.background}
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome to Cards!</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>Edit</ThemedText>
       </ThemedView>
-    </>
+    </ThemedView>
   );
 }
 
