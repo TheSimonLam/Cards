eb1bd1962c643f6e57a34cf2394434f51fec12dd
Simon Lam <simon.lam@victorianplumbing.co.uk>
2024-11-25 23:37:54 +0000

    more data adjustments

--- constants/database-examples/users-collection.ts
+++ constants/database-examples/users-collection.ts
@@ -1,12 +1,43 @@
 export const users = {
   Ninjaroni: {
-    balance: "261",
-    deckIds: ["1c8-48c-6c4-c854cc"],
+    profilePic: "123-abc-def", // This is a pre-defined image users can select. A bit like in LoL. They can unlock profile pics as they go
+    balance: "261", // Money
+    decks: [
+      {
+        deckName: "Unorganized", // Users will always have a main "Unorganized" deck where opened cards will go
+        limit: 0,
+        isPlayerMadeDeck: false,
+        cards: [
+          { cardId: "41d-c61-c36-6gie10", dateObtained: "1732372533" },
+          { cardId: "41d-c61-c36-6gie10", dateObtained: "1732372589" },
+        ],
+      },
+    ],
     friends: ["Uteki"],
   },
   Uteki: {
+    profilePic: "123-abc-def",
     balance: "531",
-    deckIds: ["vc7-3c7-753-c7531r", "c73-c75-135-75v2yx"],
+    decks: [
+      {
+        deckName: "Unorganized",
+        limit: 0,
+        isPlayerMadeDeck: false,
+        cards: [
+          { cardId: "41d-c61-c36-6gie10", dateObtained: "1732372522" },
+          { cardId: "41d-c61-c36-6gie12", dateObtained: "1732372526" },
+        ],
+      },
+      {
+        deckName: "My fire deck",
+        limit: 30,
+        isPlayerMadeDeck: true,
+        cards: [
+          { cardId: "41d-c61-c36-6gie10", dateObtained: "1732372511" },
+          { cardId: "41d-c61-c36-6gie12", dateObtained: "1732372545" },
+        ],
+      },
+    ],
     friends: ["Ninjaroni"],
   },
 };
